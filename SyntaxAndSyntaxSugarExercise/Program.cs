28258e87c1a27cd5bc7051fb15aa13758de7f89c
Garrett Chitwood <gman@acvwa.com>
2025-08-02 10:53:54 -0700

    Adjusted int and if else statements into ternary form.

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4; //int refactor
+            var response = answer < 9 ? $"{answer} is less than nine." : $"{answer} is greater than or equal to nine."; //string refactor with ternary changes.
+            Console.WriteLine(response);
         }
     }
 }
